834788cd7b64292d5226099eb3b77ba562a7547b
Bruno Venancio <brunin207.s@gmail.com>
2025-11-20 22:09:11 -0300

    Delete Projeto_EcoCycle directory

--- Projeto_EcoCycle/src/coleta/Material.java
+++
@@ -1,6 +0,0 @@
-package coleta;
-
-
-public enum Material {
-    PLASTICO, PAPEL, VIDRO, METAL
-}
--- Projeto_EcoCycle/src/usuarios/Cooperativa.java
+++
@@ -1,7 +0,0 @@
-package usuarios;
-
-public class Cooperativa extends Usuario {
-    public Cooperativa(String nome, String contato) { super(nome, contato); }
-    @Override public String getTipo() { return "COOPERATIVA"; }
-}
-
