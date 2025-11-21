632716b52f2299d3908a6f685c8c65e660517e73
Bruno Venancio <brunin207.s@gmail.com>
2025-11-20 22:11:32 -0300

    Add files via upload

---
+++ Projeto_EcoCycle/src/usuarios/Cidadao.java
@@ -0,0 +1,12 @@
+package usuarios;
+
+public class Cidadao extends Usuario {
+    public Cidadao(String nome, String contato) {
+        super(nome, contato);
+    }
+    @Override public String getTipo() {
+        return "CIDADÃO";
+    }
+}
+
+
