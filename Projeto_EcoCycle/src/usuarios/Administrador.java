f8c615e0b8f34593c5fff46bd5fc761e08db61cc
Bruno Venancio <brunin207.s@gmail.com>
2025-11-29 14:38:34 -0300

    Delete Projeto_EcoCycle directory

--- Projeto_EcoCycle/src/usuarios/Administrador.java
+++
@@ -1,9 +0,0 @@
-package usuarios;
-
-public class Administrador extends Usuario {
-    public Administrador(String nome, String contato) {
-        super(nome, contato);
-    }
-    @Override public String getTipo() {
-        return "ADMINISTRADOR"; }
-}
